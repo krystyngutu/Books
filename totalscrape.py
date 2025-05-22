86dccc970ccfd8b5cdc6ac785007abc99f628f1f
Krystyn Gutu <102545277+krystyngutu@users.noreply.github.com>
2025-05-22 15:45:39 -0400

    Update totalscrape.py

--- totalscrape.py
+++ totalscrape.py
@@ -114,5 +114,5 @@ for index, row in df.iterrows():
         continue
 
 # Save updated data
-df.to_csv("goodreads_books_all_pages_updated.csv", index=False)
+df.to_csv("goodreads_books.csv", index=False)
 print("Scraping completed. Data saved to 'goodreads_books.csv'.")
